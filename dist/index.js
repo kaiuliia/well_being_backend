5abd7b598a4e83d228a137de3691ab881b07c39c
kaiuliia <76730155+kaiuliia@users.noreply.github.com>
2023-11-01 10:07:23 +0100

    connected data base

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,35 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = require("body-parser");
 const dotenv_1 = __importDefault(require("dotenv"));
+const pg_1 = require("pg");
+const uuid_1 = require("uuid");
+const client = new pg_1.Client({ database: "wellbeing" });
+client.connect().catch(console.log);
 //For env File
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-const port = process.env.PORT || 8000;
-app.get("/", (req, res) => {
-    res.send("Welcome to 123 & TypeScript Server");
-});
+app.use((0, body_parser_1.json)());
+const port = process.env.PORT || 9090;
+app.post("/register", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { email, password } = req.body;
+    const userId = (0, uuid_1.v4)();
+    yield client.query(`INSERT INTO public.users (email, password, id) VALUES ('${email}', '${password}', '${userId}')`);
+    res.status(201).send({ userId }).end();
+}));
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,49 @@
 import express, { Request, Response, Application } from "express";
+import { json } from "body-parser";
 import dotenv from "dotenv";
+import { Client } from "pg";
+import { v4 as uuidv4 } from "uuid";
+
+const client = new Client({ database: "wellbeing" });
+client.connect().catch(console.log);
 
 //For env File
 dotenv.config();
 
 const app: Application = express();
-const port = process.env.PORT || 8000;
+app.use(json());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to 123 & TypeScript Server");
-});
+const port = process.env.PORT || 9090;
+
+interface RegisterRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+interface RegisterResponse {
+  userId: string;
+}
+
+app.post(
+  "/register",
+  async (
+    req: Request<
+      {} /*p*/,
+      RegisterResponse /*resbody - то что я должна ответить*/,
+      RegisterRequest /*reqbody - то что я принимаю*/
+    >,
+    res: Response,
+  ): Promise<void> => {
+    const { email, password } = req.body;
+    const userId = uuidv4();
+    await client.query(
+      `INSERT INTO public.users (email, password, id) VALUES ('${email}', '${password}', '${userId}')`,
+    );
+
+    res.status(201).send({ userId }).end();
+  },
+);
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
