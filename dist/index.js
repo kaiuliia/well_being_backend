1dbdd6c22671879f0be4f26555c4d006b4111b0d
kaiuliia <76730155+kaiuliia@users.noreply.github.com>
2023-11-15 11:02:38 +0100

    req works

--- dist/index.js
+++ dist/index.js
@@ -25,13 +25,39 @@ const RegisterRequestSchema = zod_1.z.object({
     email: zod_1.z.string().email(),
     password: zod_1.z.string(),
 });
+const SurveyRequestSchema = zod_1.z.object({
+    general_mood: zod_1.z.number(),
+    appetite: zod_1.z.number(),
+    sleep: zod_1.z.number(),
+    anxiety: zod_1.z.number(),
+    yourself_time: zod_1.z.number(),
+    screen_time: zod_1.z.number(),
+});
 const client = new pg_1.Client({ database: "wellbeing" });
 client.connect().catch(console.log);
 //For env File
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use((0, body_parser_1.json)());
-app.use((0, cors_1.default)());
+// READ ABOUT WHAT IS CORS AND WHY IT IS NEEDED
+app.use((0, cors_1.default)({
+    credentials: true,
+    origin: (origin, callback) => {
+        console.log(origin);
+        if (!origin)
+            return callback(null, true);
+        const allowedOrigins = [
+            "http://localhost:3000",
+            "https://wellbeing.rusanova.eu",
+        ];
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = "The CORS policy for this site does not " +
+                "allow access from the specified Origin.";
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
+}));
 app.use((0, cookie_parser_1.default)());
 const port = process.env.PORT || 9090;
 app.post("/register", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
@@ -45,15 +71,15 @@ app.post("/register", (req, res) => __awaiter(void 0, void 0, void 0, function* 
         const { name, email, password } = result.data;
         const userId = (0, uuid_1.v4)();
         yield client.query(`INSERT INTO public.users (name, email, password, id) VALUES ('${name}', '${email}', '${password}', '${userId}')`);
-        res.cookie("id", { userId });
-        res.status(201).end();
+        res.cookie("userId", userId);
+        res.status(201).send({ name: name }).end();
     }
 }));
 app.post("/login", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { email, password } = req.body;
     const { rows } = yield client.query(`SELECT * FROM public.users WHERE email = '${email}' AND password = '${password}'`);
     if (rows.length > 0) {
-        // res.cookie("id", { userId });
+        res.cookie("userId", rows[0].id);
         res
             .status(201)
             .send({ name: `${rows[0].name}.` })
@@ -61,6 +87,35 @@ app.post("/login", (req, res) => __awaiter(void 0, void 0, void 0, function* () 
     }
     else {
         res.status(404).send({ error: "Sorry, we cannot find that!" });
+    }
+}));
+app.post("/survey", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { userId } = req.cookies;
+    if (!userId) {
+        res.status(401).send({ error: "no userId" });
+        return;
+    }
+    const { rows } = yield client.query(`SELECT * FROM public.users WHERE id = '${userId}'`);
+    if (rows.length === 0) {
+        res.status(401).send({ error: "no userId" });
+        return;
+    }
+    const result = SurveyRequestSchema.safeParse(req.body);
+    if (!result.success) {
+        const error = result.error;
+        res.status(400).send({ error });
+        return;
+    }
+    else {
+        const { general_mood, appetite, sleep, anxiety, yourself_time, screen_time, } = result.data;
+        const id = (0, uuid_1.v4)();
+        yield client.query(`INSERT INTO public.survey ( id, user_id, general_mood,
+                                       appetite,
+                                       sleep,
+                                       anxiety,
+                                       yourself_time,
+                                       screen_time) VALUES ('${id}', '${userId}', '${general_mood}', '${appetite}', '${sleep}','${anxiety}','${yourself_time}','${screen_time}')`);
+        res.status(201).send({ id }).end();
     }
 }));
 app.listen(port, () => {
--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,17 @@ const RegisterRequestSchema = z.object({
   password: z.string(),
 });
 
+const SurveyRequestSchema = z.object({
+  general_mood: z.number(),
+  appetite: z.number(),
+  sleep: z.number(),
+  anxiety: z.number(),
+  yourself_time: z.number(),
+  screen_time: z.number(),
+});
+
 type RegisterRequest = z.infer<typeof RegisterRequestSchema>;
+type SurveyRequest = z.infer<typeof SurveyRequestSchema>;
 
 const client = new Client({ database: "wellbeing" });
 client.connect().catch(console.log);
@@ -23,12 +33,40 @@ dotenv.config();
 
 const app: Application = express();
 app.use(json());
-app.use(cors());
+// READ ABOUT WHAT IS CORS AND WHY IT IS NEEDED
+app.use(
+  cors({
+    credentials: true,
+    origin: (origin, callback) => {
+      console.log(origin);
+      if (!origin) return callback(null, true);
+
+      const allowedOrigins = [
+        "http://localhost:3000",
+        "https://wellbeing.rusanova.eu",
+      ];
+
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+
+      return callback(null, true);
+    },
+  }),
+);
+
 app.use(cookieParser());
 
 const port = process.env.PORT || 9090;
 
 interface RegisterResponse {
+  userId: string;
+}
+
+interface SurwayResponse {
   userId: string;
 }
 
@@ -54,7 +92,7 @@ app.post(
       await client.query(
         `INSERT INTO public.users (name, email, password, id) VALUES ('${name}', '${email}', '${password}', '${userId}')`,
       );
-      res.cookie("id", { userId });
+      res.cookie("userId", userId);
       res.status(201).send({ name: name }).end();
     }
   },
@@ -76,13 +114,68 @@ app.post(
     );
 
     if (rows.length > 0) {
-      // res.cookie("id", { userId });
+      res.cookie("userId", rows[0].id);
       res
         .status(201)
         .send({ name: `${rows[0].name}.` })
         .end();
     } else {
       res.status(404).send({ error: "Sorry, we cannot find that!" });
+    }
+  },
+);
+
+app.post(
+  "/survey",
+  async (
+    req: Request<
+      {} /*p*/,
+      SurwayResponse /*resbody - то что я должна ответить*/,
+      SurveyRequest /*reqbody - то что я принимаю*/
+    >,
+    res: Response,
+  ): Promise<void> => {
+    const { userId } = req.cookies;
+
+    if (!userId) {
+      res.status(401).send({ error: "no userId" });
+      return;
+    }
+
+    const { rows } = await client.query(
+      `SELECT * FROM public.users WHERE id = '${userId}'`,
+    );
+    if (rows.length === 0) {
+      res.status(401).send({ error: "no userId" });
+      return;
+    }
+    const result = SurveyRequestSchema.safeParse(req.body);
+    if (!result.success) {
+      const error = result.error;
+      res.status(400).send({ error });
+      return;
+    } else {
+      const {
+        general_mood,
+        appetite,
+        sleep,
+        anxiety,
+        yourself_time,
+        screen_time,
+      } = result.data;
+
+      const id = uuidv4();
+
+      await client.query(
+        `INSERT INTO public.survey ( id, user_id, general_mood,
+                                       appetite,
+                                       sleep,
+                                       anxiety,
+                                       yourself_time,
+                                       screen_time) VALUES ('${id}', '${userId}', '${general_mood}', '${appetite}', '${sleep}','${anxiety}','${yourself_time}','${screen_time}')`,
+      );
+
+      res.status(201).send({ id }).end();
     }
   },
 );
