b5012d002f8e24aaa1e07932d911395977434193
Yulia R. <76730155+kaiuliia@users.noreply.github.com>
2024-09-19 20:34:45 +0200

    Use connectionString from env

--- src/database/client.ts
+++ src/database/client.ts
@@ -2,5 +2,5 @@ import { Client, types } from "pg";
 
 types.setTypeParser(types.builtins.DATE, (val) => val);
 
-export const client = new Client({ database: "wellbeing" });
+export const client = new Client({ connectionString: process.env.POSTGRES_URL, database: "wellbeing" });
 client.connect().catch(console.log);
